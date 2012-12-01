37130f37f4e64d4f10ecf356ab1291541e85bad3
Cameron <rogerdodger111@internode.on.net>
2012-12-01 17:04:27 +1030

    update script finalised(?)

--- eznguide_update.pl
+++ eznguide_update.pl
@@ -1,6 +1,13 @@
 #!/usr/bin/env perl
 
+# Check if the github repo has been updated, 
+# and rebuild the site if it has
+
 use FindBin '$Bin';
 chdir($Bin);
 
-@a = `git pull git://github.com/RogerDodger/EznGuide.git`;
+my $stdout = `git pull git://github.com/RogerDodger/EznGuide.git master`;
+
+if( index( $stdout, 'Already up-to-date.' ) != -1 ) {
+	`./eznguide_build.pl`;
+}
