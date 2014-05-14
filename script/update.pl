c15c3caab4674aa35baa5704bc23e3b3971adcd5
RogerDodger <roger.dodger.thor@gmail.com>
2014-05-14 20:18:54 +0930

    final cleanup

--- script/update.pl
+++ script/update.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/env perl
 
-# Check if the github repo has been updated, 
+# Check if the github repo has been updated,
 # and rebuild the site if it has
 
 use FindBin '$Bin';
-chdir($Bin);
+chdir "$Bin/..";
 
 my $stdout = `git pull git://github.com/RogerDodger/EznGuide.git master`;
 
 if( $stdout ne "Already up-to-date.\n" ) {
-	exec 'perl eznguide_build.pl';
+	exec 'perl script/build.pl';
 }
